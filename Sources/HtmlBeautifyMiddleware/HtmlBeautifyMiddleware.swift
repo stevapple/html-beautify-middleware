9b2178e7a50f9724a6b58a96f79530c6fd79f5c2
stevapple <stevapple@icloud.com>
2021-10-24 02:09:39 +0800

    Add documentation for source code

--- Sources/HtmlBeautifyMiddleware/HtmlBeautifyMiddleware.swift
+++ Sources/HtmlBeautifyMiddleware/HtmlBeautifyMiddleware.swift
@@ -1,10 +1,19 @@
 import SwiftSoup
 import Vapor
 
+/// A ``Middleware`` to beautify the served HTML with `SwiftSoup`.
 public struct HtmlBeautifyMiddleware: Middleware {
+    /// The length of indents in space.
     public let indent: UInt
+
+    /// The ``MediaType``s to be beautified.
     public let mediaTypes: [MediaType]
 
+    /// Create a `HtmlBeautifyMiddleware`.
+    ///
+    /// - Parameters:
+    ///   - indent: The length of the indent in space.  Defaults to `2`.
+    ///   - types: An array of ``MediaType``s to be beautified.  Defaults to `[.html]`.
     public init(indent: UInt = 2, accept types: [MediaType] = [.html]) {
         self.indent = indent
         self.mediaTypes = types
@@ -32,6 +41,7 @@ public struct HtmlBeautifyMiddleware: Middleware {
         }
     }
 
+    /// Represents the media type from HTTP's `Content-Type` header, defined in [rfc2045](https://tools.ietf.org/html/rfc2045#section-5).
     public struct MediaType: RawRepresentable, ExpressibleByStringLiteral, CustomStringConvertible {
         public let rawValue: String
 
@@ -49,14 +59,19 @@ public struct HtmlBeautifyMiddleware: Middleware {
     }
 }
 
+/// Convenience helpers for ``HtmlBeautifyMiddleware.MediaType``.
 extension HtmlBeautifyMiddleware.MediaType {
+    /// HTML media type.
     public static let html = Self(rawValue: "text/html")
+    /// XML media type.
     public static let xml = Self(rawValue: "application/xml")
 
+    /// Custom application media type.
     public static func application(_ type: String) -> Self {
         Self(rawValue: "application/\(type)")
     }
 
+    /// Custom text media type.
     public static func text(_ type: String) -> Self {
         Self(rawValue: "text/\(type)")
     }
