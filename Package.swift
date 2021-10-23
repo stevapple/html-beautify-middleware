8474c898add274ce57e249a3586d2e1fff502e07
stevapple <stevapple@icloud.com>
2021-10-24 02:48:16 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -9,31 +8,27 @@ let package = Package(
         .macOS(.v10_15),
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "HtmlBeautifyMiddleware",
             targets: ["HtmlBeautifyMiddleware"]
         ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.0.0"),
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "HtmlBeautifyMiddleware",
             dependencies: [
-                "SwiftSoup",
+                .product(name: "SwiftSoup", package: "SwiftSoup"),
                 .product(name: "Vapor", package: "vapor"),
             ]
         ),
         .testTarget(
             name: "HtmlBeautifyMiddlewareTests",
             dependencies: [
-                "HtmlBeautifyMiddleware",
+                .target(name: "HtmlBeautifyMiddleware"),
                 .product(name: "XCTVapor", package: "vapor"),
             ]
         ),
